a2230822e60d3da6fdbcdfb7b73eab776bfb6d16
Nkosi <nko12@comcast.net>
2016-02-15 17:09:54 -0500

    Added the team logo. Added the RoverClient class to the RoverComm library.

---
+++ RoverComm/RoverClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Sockets;
+
+namespace RoverComm
+{
+    /// <summary>
+    /// The primary RoverClient class. This is used to start and stop communication with the robot, send and receive
+    /// data to and from the robot, and send commands to the robot.
+    /// </summary>
+    public class RoverClient
+    {
+        #region Class Variables
+        /// <summary>
+        /// The primary TCP client to use for communication with robot server.
+        /// </summary>
+        private TcpClient client;
+
+        #endregion
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public RoverClient()
+        {
+            
+        }
+    }
+}
