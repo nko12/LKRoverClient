2281e93cb23241c878ef9964786ce662d91e2fa2
Nkosi <nko12@comcast.net>
2016-03-01 00:07:51 -0500

    Figured out how to read Xbox Controller input on a separate thread, and then invoke controls to do something on Xbox Controller input.

--- LKRoverApp/WindowMain.xaml.cs
+++ LKRoverApp/WindowMain.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,42 +23,52 @@ namespace LKRoverApp
     public partial class WindowMain : Window
     {
         xbox_controller xbox = new xbox_controller();
-               
 
         public WindowMain()
         {
-            
+
             InitializeComponent();
+            this.Loaded += WindowMain_Loaded;
+        }
+
+        private void WindowMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread controllerThread = new Thread(XboxControllerThread);
+            controllerThread.Name = "Xbox Controller Thread";
+            controllerThread.IsBackground = true;
+            controllerThread.SetApartmentState(ApartmentState.STA);
+            controllerThread.Start();
+
             
         }
 
         private void MenuItemXboxDiagram_OnClick(object sender, RoutedEventArgs e)
         {
-            WindowXboxDiagram win = new WindowXboxDiagram {Owner = this};
+            WindowXboxDiagram win = new WindowXboxDiagram { Owner = this };
             win.ShowDialog();
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {    
-        
+        {
+
         }
 
         private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            
-            
-           
+
+            test_controller();
+
         }
-        
+
         private void test_controller()
         {
             xbox.buttons();
-            if(xbox.xbox_a != ButtonState.Pressed)
+            if (xbox.xbox_a != ButtonState.Pressed)
             {
 
                 textBox1.Background = Brushes.Red;
@@ -68,41 +79,77 @@ namespace LKRoverApp
             }
         }
 
+
+        private void XboxControllerThread()
+        {
+            GamePadState lastState = GamePad.GetState(PlayerIndex.One);
+            while (true)
+            {
+                //Thread.Sleep(500);
+
+                GamePadState currentState = GamePad.GetState(PlayerIndex.One);
+                //if (currentState.PacketNumber == lastState.PacketNumber)
+                //    continue;
+
+                if (currentState.Buttons.A == ButtonState.Pressed && lastState.Buttons.A == ButtonState.Released)
+                {
+                    //The button has been pressed.
+                    InvokeSetControlBackColor(textBox1, Colors.Green);
+                }
+                else if (currentState.Buttons.A == ButtonState.Released && lastState.Buttons.A == ButtonState.Pressed)
+                {
+                    //The button has been released.
+                    InvokeSetControlBackColor(textBox1, Colors.Red);
+                }
+
+
+                lastState = currentState;
+            }
+        }
+
+        private void InvokeSetControlBackColor(Control c, Color color)
+        {
+            c.Dispatcher.Invoke(delegate()
+            {
+                c.Background = new SolidColorBrush(color);
+            });
+        }
     }
 
 }
 
-    public partial class xbox_controller
+public class xbox_controller
+{
+    public void rumble_on()
     {
-        public void rumble_on()
-        {
-            GamePad.SetVibration(PlayerIndex.One, 1, 1);
-        }
-        
-        public void rumble_off()
-        {
-            GamePad.SetVibration(PlayerIndex.One, 0, 0);
-        }
-        //variables for the xbox buttons (are either buttonstate.pressed  or buttonstate.released)
-        public ButtonState xbox_a;
-        public ButtonState xbox_b;
-        public ButtonState xbox_x;
-        public ButtonState xbox_y;
-        public ButtonState xbox_start;
-        public ButtonState xbox_back;
-        public ButtonState xbox_home;
-        public ButtonState xbox_rightbumper;
-        public ButtonState xbox_leftbumper;
-        public ButtonState left_joystick_press;
-        public ButtonState right_joystick_press;
-    
-        public void buttons()
-        {
+        GamePad.SetVibration(PlayerIndex.One, 1, 1);
+    }
+
+    public void rumble_off()
+    {
+        GamePad.SetVibration(PlayerIndex.One, 0, 0);
+    }
+    //variables for the xbox buttons (are either buttonstate.pressed  or buttonstate.released)
+    public ButtonState xbox_a;
+    public ButtonState xbox_b;
+    public ButtonState xbox_x;
+    public ButtonState xbox_y;
+    public ButtonState xbox_start;
+    public ButtonState xbox_back;
+    public ButtonState xbox_home;
+    public ButtonState xbox_rightbumper;
+    public ButtonState xbox_leftbumper;
+    public ButtonState left_joystick_press;
+    public ButtonState right_joystick_press;
+
+    public void buttons()
+    {
         //retrieves the buttonstate of all buttons
         PlayerIndex player = PlayerIndex.One;
-
-        xbox_a = GamePad.GetState(player).Buttons.A;
-        xbox_b = GamePad.GetState(player).Buttons.B;
+        var gamePadState = GamePad.GetState(player);
+        
+        xbox_a = gamePadState.Buttons.A;
+        xbox_b = gamePadState.Buttons.B;
         xbox_x = GamePad.GetState(player).Buttons.X;
         xbox_y = GamePad.GetState(player).Buttons.Y;
         xbox_start = GamePad.GetState(player).Buttons.Start;
@@ -112,6 +159,6 @@ namespace LKRoverApp
         xbox_leftbumper = GamePad.GetState(player).Buttons.LeftShoulder;
         var left_joystick_press = GamePad.GetState(player).Buttons.LeftStick;  //not values of axis (only pressed value)
         var right_joystick_press = GamePad.GetState(player).Buttons.RightStick; // not values of axis (only pressed value)
-        }
+    }
 
 }
